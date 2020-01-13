c61c962a13fe0ed44d50dddad771d78459a36570
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-01-13 15:59:22 +0000

    corrected generation of lists to not inclue strings

--- firstfile.py
+++ firstfile.py
@@ -2,9 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 pathroute = raw_input("path to route\n> ")
-route= np.genfromtxt(pathroute, delimiter=",",dtype='unicode')
+routegentext= np.genfromtxt(pathroute, delimiter=",",dtype='unicode')
 pathpassenger = raw_input("path to passenger\n> ")
-route= np.genfromtxt(pathpassenger, delimiter=",",dtype='unicode')
+passgentext= np.genfromtxt(pathpassenger, delimiter=",",dtype='unicode')
+
+
+route=[]
+for i in range(len(route)):
+    route.append((int(route[i,0]),int(route[i,1]),(route[i,2])))
+    
+passengers=[]    
+for i in range(len(passengers)):
+    passengers.append(((passgentext[i,0],passgentext[i,1]),(passgentext[i,2],passgentext[i,3]),(passgentext[i,4])))
+
 
 def timetable(route):
     '''
