090c389e9dca36378cfbb41a177eab0a5a6ec0b4
sukrire <sandromarcus@hotmail.co.uk>
2020-01-18 17:30:59 +0000

    trying something new, not working great so far

--- Journey.py
+++ Journey.py
@@ -1,46 +1,50 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import Passenger
+from Passenger import Passenger 
 import Route
 
-class Journey:  
+class Journey(Passenger):  
     
-    def __init__(self,route,passenger,passno, speed=10):
-        self.route = Route.Route(route,speed)
-        self.passenger = Passenger.Passenger(passenger,passno)
-        self.timetable= Route.Route.timetable(self.route)
-        self.stops = [step[2] for step in self.timetable if step[0]]
+	def __init__(self,route, passen=None ,speed = None, passno=0 ,start=None,end=None,pace=None, pathpassenger = None):
+		super().__init__(start=None,end=None,pace=None,pathpassenger=None)
+		self.route = Route.Route(route)
+		self.stops = [step[2] for step in self.route.timetable() if step[0]]
         
-    def plot_bus_load(self):
-        self.value = {value[2]:0 for value in self.timetable if value[0]}
-        self.stoplabels = [step[0] for step in self.timetable if step[0]]
-        for testpass in range(len(self.passenger.passengers)):
-            self.value[self.passenger_trip(testpass)[0][1]] += 1
-            self.value[self.passenger_trip(testpass)[1][1]] -= 1
-        for i, stop in enumerate(self.value):
-            if i > 0:
-                self.value[stop] += self.value[prev]
-            prev = stop
-        fig, ax = plt.subplots()
-        ax.step(range(len(self.value)), list(self.value.values()), where='post')
-        ax.set_xticks(range(len(self.stops)))
-        ax.set_xticklabels(list(self.stoplabels))
-        plt.show()
-        
-    def passenger_trip(self,testpass):
-        # calculate closer stops
-        ## to start
-        self.distances = [(math.sqrt((self.route.xcoord[stop] - self.passenger.passengers[testpass][0][0])**2 + (self.route.ycoord[stop] - self.passenger.passengers[testpass][0][1])**2), stop) for stop in self.stops]
-        closer_start = min(self.distances)
-        ## to endRoute.
-        self.distances = [(math.sqrt((self.route.xcoord[stop] - self.passenger.passengers[testpass][1][0])**2 + (self.route.ycoord[stop] - self.passenger.passengers[testpass][1][1])**2), stop) for stop in self.stops]
-        closer_end = min(self.distances)
-        return (closer_start, closer_end)
+	def plot_bus_load(self):
+		# calculate closer stops
+		## to start
+		distances = [(math.sqrt((self.route.xcoord[stop] - self.passenger.passengers[testpass][0][0])**2 + (self.route.ycoord[stop] - self.passenger.passengers[testpass][0][1])**2), stop) for stop in self.stops]
+		closer_start = min(self.distances)
+		## to endRoute.
+		distances = [(math.sqrt((self.route.xcoord[stop] - self.passenger.passengers[testpass][1][0])**2 + (self.route.ycoord[stop] - self.passenger.passengers[testpass][1][1])**2), stop) for stop in self.stops]
+		closer_end = min(self.distances)
+		self.value = {value[2]:0 for value in self.route.timetable() if value[0]}
+		self.stoplabels = [step[0] for step in self.route.timetable() if step[0]]
+		for testpass in range(len(self.passenger.passengers)):
+			self.value[self.passenger_trip(testpass)[0][1]] += 1
+			self.value[self.passenger_trip(testpass)[1][1]] -= 1
+		for i, stop in enumerate(self.value):
+			if i > 0:
+				self.value[stop] += self.value[prev]
+			prev = stop
+		fig, ax = plt.subplots()
+		ax.step(range(len(self.value)), list(self.value.values()), where='post')
+		ax.set_xticks(range(len(self.stops)))
+		ax.set_xticklabels(list(self.stoplabels))
+		plt.show()
+		
+	def passenger_trip(self,):
+		distances = [(math.sqrt((self.route.xcoord[stop] - self.specifystart[0])**2 + (self.route.ycoord[stop] - self.specifstart[1])**2), stop) for stop in self.stops]
+		closer_start = min(self.distances)
+		## to endRoute.
+		distances = [(math.sqrt((self.route.xcoord[stop] - self.passenger.passengers[testpass][1][0])**2 + (self.route.ycoord[stop] - self.passenger.passengers[testpass][1][1])**2), stop) for stop in self.stops]
+		closer_end = min(self.distances)
+		return (closer_start, closer_end)
 
-    def passenger_trip_time(self,testpass):
-        bus_times = [row[1] for row in self.timetable]
-        bus_travel = bus_times[self.passenger_trip(testpass)[1][1]] - bus_times[self.passenger_trip(testpass)[0][1]]
-        walk_travel = self.passenger_trip(testpass)[0][0] * self.passenger.pace + self.passenger_trip(testpass)[1][0] * self.passenger.pace
-        return bus_travel + walk_travel
+	def passenger_trip_time(self,testpass):
+		bus_times = [row[1] for row in self.timetable]
+		bus_travel = bus_times[self.passenger_trip(testpass)[1][1]] - bus_times[self.passenger_trip(testpass)[0][1]]
+		walk_travel = self.passenger_trip(testpass)[0][0] * self.passenger.pace + self.passenger_trip(testpass)[1][0] * self.passenger.pace
+		return bus_travel + walk_travel
     
--- Passenger.py
+++ Passenger.py
@@ -3,16 +3,22 @@ from read_passengers import Read_passengers
 
 class Passenger:
 
-    def __init__(self, pathpassenger="passengers.csv", passno=0 ,start=[],end=[],pace=[]):
-        self.start = []
-        self.end = []
-        self.pace = []
+	def __init__(self, pathpassenger = None, passno=0 ,start=None,end=None,pace=None):
+		self.start = []
+		self.end = []
+		self.pace = []
 		self.specifystart = start
-        self.specifyend = end
-        self.specifypace = pace
-        self.passengers,self.passenger_id=Read_passengers(pathpassenger)
-        self.start,self.end,self.pace = self.passengers[passno]
+		self.specifyend = end
+		self.specifypace = pace
+		self.pathpassenger = pathpassenger
+		if self.pathpassenger is not None:
+			self.passengers=Read_passengers(pathpassenger)
+			self.start,self.end,self.pace = self.passengers[passno]
    
-    def walktime(self):
-        return np.sqrt((self.start[0]-self.end[0])**2+(self.start[1]-self.end[1])**2)*self.pace
+	def walktime(self, pathpassenger):
+		if pathpassenger is None:
+			self.start = self.specifystart
+			self.end = self.specifyend
+			self.pace = self.specifypace
+		return np.sqrt((self.start[0]-self.end[0])**2+(self.start[1]-self.end[1])**2)*self.pace
     
