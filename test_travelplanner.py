e56bd1d3f3bd414ff165edb2bb2d0f3c7c2685fe
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-01-19 16:45:03 +0000

    now have a working pytest file

--- test_travelplanner.py
+++ test_travelplanner.py
@@ -3,9 +3,10 @@ from Route import Route
 from Journey import Journey
 import pytest
 
+
 #passenger class tests
 
-def Passenger():
+def test_Passenger():
     testpassenger = Passenger(start=(0,0),end=(3,4),pace=10)
     testpassengers = Passenger("passenger.csv")
     assert testpassenger.start  == []
@@ -16,37 +17,37 @@ def Passenger():
     assert testpassenger.specifypace == 10
     assert testpassengers.start == (2,3)
     assert testpassengers.end == (24,0)
-    assert testpassengers.pace == 10
+    assert testpassengers.pace == 12
     assert testpassengers.specifystart == None
     assert testpassengers.specifyend == None
     assert testpassengers.specifypace == None
     
 def test_walk_time():
-    testwalktime= Passenger(start=(0,0),end=(3,4),pace=10)
-    assert testwalktime.walk_time() == 50
+    testwalktime = Passenger(start=(0,0),end=(3,4),pace=10)
+    assert testwalktime.walktime() == 50
     
 #route class tests    
     
-def Route():
+def test_Route():
     testroute = Route("route.csv")
     assert testroute.bstop[0] == 'A'
     #assert(Route("route_wrong.csv")) =  'Diagonal route'   
     
-def timetimetable():
+def test_timetimetable():
     testroute = Route("route.csv")
     assert(testroute.timetable()[12]) == ('B', 120, 12)
     
 #journey class tests
 
-def Journey():
+def test_Journey():
     journeytest=Journey("route.csv","passenger.csv",speed=10)
     assert journeytest.stops == [0, 12, 17, 18, 19, 22, 25]
     
-def travel_time():
+def test_travel_time():
     jtesttt=Journey("route.csv","passenger.csv")
     assert jtesttt.travel_time()[1] == ['266.4432397340942', '0']
 
-def print_time_stats():
+def test_print_time_stats():
     jtestpts = Journey("route.csv","passenger.csv")
     jtestpts.print_time_stats()
     assert (jtestpts.total_time_on_bus/jtestpts.bus_pass) == 0
