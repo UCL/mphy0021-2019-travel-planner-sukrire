eb8d3f6fbb27c118a989cd6956544eb180329eec
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-01-19 17:56:38 +0000

    i think i changed some stuff, lets hope travis likes it

--- travelplanner/tests/test_travelplanner.py
+++ travelplanner/tests/test_travelplanner.py
@@ -2,6 +2,7 @@ from travelplanner.read_passengers import Read_passengers,Read_route
 from travelplanner.Passenger import Passenger
 from travelplanner.Route import Route
 from travelplanner.Journey import Journey
+import numpy as np
 import pytest
 
 #need to write tests for read passengers
