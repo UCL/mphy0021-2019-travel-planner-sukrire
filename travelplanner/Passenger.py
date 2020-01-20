4fb63c6ef126827f8db5c515e0698e0099518e67
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-01-20 14:01:59 +0000

    should all be in pep8 now #14

--- travelplanner/Passenger.py
+++ travelplanner/Passenger.py
@@ -1,9 +1,11 @@
 import numpy as np
 from travelplanner.read_passengers import Read_passengers
 
+
 class Passenger:
 
-    def __init__(self, pathpassenger=None, passno=0 ,start=None,end=None,pace=None):
+    def __init__(self, pathpassenger=None, passno=0, start=None, end=None,
+                 pace=None):
         self.start = []
         self.end = []
         self.pace = []
@@ -11,14 +13,15 @@ class Passenger:
         self.specifyend = end
         self.specifypace = pace
         if pathpassenger is not None:
-            self.passengers=Read_passengers(pathpassenger)
-            self.start,self.end,self.pace = self.passengers[passno]
+            self.passengers = Read_passengers(pathpassenger)
+            self.start, self.end, self.pace = self.passengers[passno]
 
     def walktime(self, passno=None):
         if passno is None:
             walkstart = self.specifystart
             walkend = self.specifyend
-            walkpace = self.specifypace	
+            walkpace = self.specifypace
         else:
-            walkstart,walkend,walkpace = self.passengers[passno]   
-        return np.sqrt((walkstart[0]-walkend[0])**2+(walkstart[1]-walkend[1])**2)*walkpace
+            walkstart, walkend, walkpace = self.passengers[passno]
+        return np.sqrt((walkstart[0] - walkend[0])**2 +
+                       (walkstart[1] - walkend[1])**2)*walkpace
